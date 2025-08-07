a069378727698695e482ae99a509f226a89aa567
BCA 39 <BCA 39@NIPE-LAB3-029>
2025-08-07 12:33:27 +0530

    Add project files.

---
+++ squareofno/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace square
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number:");
+            if (!int.TryParse(Console.ReadLine(), out int num))
+            {
+                Console.WriteLine("Invalid number");
+                return;
+            }
+            Console.WriteLine($"{num} = {num*num}");
+        }
+
+    }
+}
+
+
+
+
+
